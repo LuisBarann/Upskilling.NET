10011b7d5521ead635c7cc1193f8c75f23767cc6
Luis Barann <luisbarann@gmail.com>
2022-03-08 17:41:03 -0300

    API Rests y DAO

---
+++ WebAppArticulos/DAO/ConexionBD.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppArticulos.DAO
+{
+    public class ConexionBD
+    {
+        private const string CADENA = "SServer=DESKTOP-C0PCF1H;Database=ArticulosDBAccenture;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        public static SqlConnection connection = new SqlConnection(CADENA);
+    }
+}
